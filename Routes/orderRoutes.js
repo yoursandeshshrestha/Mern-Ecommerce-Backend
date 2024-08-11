17b34299dd56f534f896abbe51d00c6e1d258721
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-08-11 16:04:27 +0530

    getOrderedProductsBySeller

--- Routes/orderRoutes.js
+++ Routes/orderRoutes.js
@@ -4,9 +4,11 @@ const { Authenticate } = require("../middleware/authMiddleware");
 const {
   newOrder,
   getOrderedProductsByCustomer,
+  getOrderedProductsBySeller,
 } = require("../controllers/orderController");
 
 router.post("/create", Authenticate, newOrder);
 router.get("/", Authenticate, getOrderedProductsByCustomer);
+router.get("/:id", Authenticate, getOrderedProductsBySeller);
 
 module.exports = router;
